2cfb74c45d8d53f1414060791c466bcea3defb96
Lucas <l.aulen@ludus-academie.com>
2019-12-10 17:47:30 +0100

    azer

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+
+    public GameObject Bulletprefab;
+
+    public Vector3 center;
+    public Vector3 size;
+    public float fltTimer;
+
+    bool timerActive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBullet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive)
+        {
+            fltTimer += Time.deltaTime;
+            if (fltTimer >= 2)
+            {
+                SpawnBullet();
+                fltTimer = 0;
+            }
+        }
+    }
+
+    public void SpawnBullet()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2,size.x/2), Random.Range(-size.y / 2, size.y / 2), 20);
+        Instantiate(Bulletprefab, pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
---
+++ Assets/Scripts/HandMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rigidbody;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    //Fonction qui change la position de la boule quand on bouge le bras
+    void PositionMain(int nX, int nY)
+    {
+        
+    }
+}
